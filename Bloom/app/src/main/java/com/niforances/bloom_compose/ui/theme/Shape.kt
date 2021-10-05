b81776389c730fa1abb94402d93f865d57fec946
Niforances <niforances@gmail.com>
2021-10-05 13:58:51 +0900

    [Update] Shape.kt

--- Bloom/app/src/main/java/com/niforances/bloom_compose/ui/theme/Shape.kt
+++ Bloom/app/src/main/java/com/niforances/bloom_compose/ui/theme/Shape.kt
@@ -5,7 +5,10 @@ import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
+
+    // small: SnackBar, Image, TextField, Card
     small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(4.dp),
-    large = RoundedCornerShape(0.dp)
+
+    // medium: Button
+    medium = RoundedCornerShape(24.dp),
 )
